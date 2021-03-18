4555046fe150d674518bcf9d005f387b3d421270
pezhmanparsaee <p.parsaee@gmail.com>
2021-03-18 22:23:35 +0330

    enhance person proxy example

--- proxy/ex4/person.js
+++ proxy/ex4/person.js
@@ -2,12 +2,32 @@ const person = {
   name: "Jack",
   place: "Chennai",
 };
+
 const handler = {
   get: function (target, prop) {
     return prop in target ? target[prop] : "Invalid Prop";
   },
+  set: function (target, prop, value) {
+    if (prop === "age") {
+      if (value < 100) {
+        target[prop] = value;
+      } else {
+        throw new RangeError("Invalid Age");
+      }
+    }
+  }
 };
 
 const newPerson = new Proxy(person, handler);
-console.log(newPerson.name, newPerson.place);
-console.log(newPerson.age);
+console.log(`newPerson.name: ${newPerson.name}, newPerson.place: ${newPerson.place}`);
+console.log(`person.name: ${person.name}, person.place: ${person.place}`);
+console.log(`newPerson.age: ${newPerson.age}`);
+console.log(`person.age: ${person.age}`);
+
+
+newPerson.age = 25;
+console.log(`newPerson.age: ${newPerson.age}`);
+console.log(`person.age: ${person.age}`);
+
+newPerson.age = 110;
+
